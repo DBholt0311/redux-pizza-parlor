30d72a97e7bf1757088136c052892ad49f298bc4
Daniel Holt <holt.daniel.b@gmail.com>
2024-02-06 17:59:00 -0500

    Inputs and buttons wrapped in form tag, submit function created

--- src/components/customerInfoForm/customerInfoForm.jsx
+++ src/components/customerInfoForm/customerInfoForm.jsx
@@ -1,16 +1,44 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
-function CustomerInfo () {
-    return (
-        <div>
-            <p>Step 2 Customer Information</p>
-            <input placeholder="Name"></input>
-            <input placeholder="Street Address"></input>
-            <input placeholder="City"></input>
-            <input placeholder="Zip"></input>
-            <button>Delivery</button>
-            <button>Pick-Up</button>
-        </div>
-    );
+function CustomerInfo() {
+  const [inputName, setInputName] = useState("");
+  const [inputAddress, setInputAddress] = useState("");
+  const [inputCity, setInputCity] = useState("");
+  const [inputZip, setInputZip] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatchEvent({
+      type: "CUSTOMER_INFO_ADD",
+      payload: {
+        name: inputName,
+        address: inputAddress,
+        city: inputCity,
+        zip: inputZip,
+      },
+    });
+
+    setInputName("");
+    setInputAddress("");
+    setInputCity("");
+    setInputZip("");
+  };
+
+  return (
+    <div>
+      <h1>Step 2 Customer Information</h1>
+      <form onSubmit={handleSubmit}>
+        <input placeholder="Name" id="name" value={inputName} />
+        <input placeholder="Street Address" id="address" value={inputAddress} />
+        <input placeholder="City" id="city" value={inputCity} />
+        <input placeholder="Zip" id="zip" value={inputZip} />
+        <button>Delivery</button>
+        <button>Pick-Up</button>
+        <button type="submit">Next</button>
+      </form>
+    </div>
+  );
 }
 
 export default CustomerInfo;
