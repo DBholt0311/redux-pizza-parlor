176a2d7ae9900b7c60af5b2d366a631d20e27589
Daniel Holt <holt.daniel.b@gmail.com>
2024-02-06 17:33:07 -0500

    component folder, and file created. basic inputs and buttons added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import CustomerInfo from '../customerInfoForm/customerInfoForm';
 import './App.css';
 
 function App() {
@@ -12,7 +13,9 @@ function App() {
   
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
-  
+    <footer>
+    <CustomerInfo/>
+    </footer>
     </div>
   );
 }
---
+++ src/components/customerInfoForm/customerInfoForm.jsx
@@ -0,0 +1,16 @@
+
+function CustomerInfo () {
+    return (
+        <div>
+            <p>Step 2 Customer Information</p>
+            <input placeholder="Name"></input>
+            <input placeholder="Street Address"></input>
+            <input placeholder="City"></input>
+            <input placeholder="Zip"></input>
+            <button>Delivery</button>
+            <button>Pick-Up</button>
+        </div>
+    );
+}
+
+export default CustomerInfo;
