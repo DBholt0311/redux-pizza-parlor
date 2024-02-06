e96cdfb05c5aba2f588f61785c9a75fba2c3ed93
Jan Michael Biyo <janmichael.biyo@gmail.com>
2024-02-05 21:26:37 -0500

    added web page and route will now work on posting info to the rest

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
+import Checkout from '../Checkout/Checkout';
+import Pizza from '../Pizza/Pizza';
 
 function App() {
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
-      </header>
-  
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-  
+    <div>
+      {/* <header className="App-header">
+        <h1 className="App-title">Prime Pizza</h1>
+      </header> */}
+      <Router>
+        {/* <img src="images/pizza_photo.png" />
+        <p>Pizza is great.</p> */}
+        <Route path="/" exact>
+          <Pizza />
+        </Route>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+      </Router>
     </div>
   );
 }
