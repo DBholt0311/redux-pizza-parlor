ec3078b3a76b420cb07c23838cde09490e1dcdc1
Daniel Holt <holt.daniel.b@gmail.com>
2024-02-10 11:05:48 -0500

    store.js file updated

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,18 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
-import logger from 'redux-logger';
-
-// Be sure to replace this reducer! 🙂
-const someReducer = (state = [], action) => {
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import logger from "redux-logger";
+// Be sure to replace this reducer! :slightly_smiling_face:
+const pizza = (state = [], action) => {
+  //dispatch call
+  if (action.type === "SET_PIZZA_LIST") {
+    //return payload
+    return action.payload;
+  }
   return state;
-}
-
+};
 const store = createStore(
   combineReducers({
-    someReducer, // 👈 Be sure to replace this, too!
+    pizza, // :point_left: Be sure to replace this, too!
   }),
-  applyMiddleware(logger),
+  applyMiddleware(logger)
 );
-
-
 export default store;
