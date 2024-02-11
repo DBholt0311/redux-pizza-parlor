0e1f90395bccc425cfe5936065a07e1996b989bd
Jan Michael Biyo <janmichael.biyo@gmail.com>
2024-02-11 12:19:36 -0500

    added some now info the pizza is now printing

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import axio from 'axios';
+import axios from 'axios';
 
 function Checkout() {
   const history = useHistory();
-  const customerInfo = useSelector((state) => state.customerInfo);
+  // const customerInfo = useSelector((state) => state.customerInfo);
   const pizza = useSelector((state) => state.pizza);
 
   const handleCompleteOrder = (event) => {
     event.preventDefault();
 
     axios
-      .post('/api/pizza', { pizza, cost })
+      .post('/api/pizza', { pizza })
       .then((response) => {
-        console.log({ pizza, cost });
+        console.log({ pizza });
       })
       .catch((error) => {
         console.log('issue with post', error);
       });
 
-    axios
-      .post('/api/order', {
-        customer_name,
-        street_address,
-        city,
-        zip,
-        type,
-        total,
-        pizzas,
-      })
-      .then((response) => {
-        console.log({ total });
-      })
-      .catch((error) => {
-        console.log('issue with pizza post', error);
-      }).then;
+    // axios
+    //   .post('/api/order', {
+    //     customer_name,
+    //     street_address,
+    //     city,
+    //     zip,
+    //     type,
+    //     total,
+    //     pizzas,
+    //   })
+    //   .then((response) => {
+    //     console.log({ total });
+    //   })
+    //   .catch((error) => {
+    //     console.log('issue with pizza post', error);
+    //   })
 
     history.push('/');
   };
@@ -46,21 +46,30 @@ function Checkout() {
         <h1 className="Checkout-title">Prime Pizza</h1>
       </header>
       <h2>Step 3: Checkout</h2>
-      {customerInfo.map(
+      {/* {customerInfo.map(
         (customer) => (
           customer.name, customer.address, customer.city, customer.zip
         )
-      )}
+      )} */}
+
       <table>
-        <tr>
-          <th>Name</th>
-          <th>Cost</th>
-        </tr>
-        <th>{pizza.name}</th>
-        <th>{pizza.cost}</th>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pizza.map((pizza) => (
+            <tr key={pizza.id}>
+              <th>{pizza.name}</th>
+              <th>{pizza.price}</th>
+            </tr>
+          ))}
+        </tbody>
       </table>
 
-      <p1>Total: {pizza.cost}</p1>
+      <p>Total: {pizza.price}</p>
       <button onClick={handleCompleteOrder}>Checkout</button>
     </div>
   );
