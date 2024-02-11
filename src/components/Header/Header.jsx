c62c91a5d0dbc7ff439a07b60964f3db85953331
Jan Michael Biyo <janmichael.biyo@gmail.com>
2024-02-11 11:32:34 -0500

    added header to my feature

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+function Header({ title }) {
+  return (
+    <div>
+      <h1 className="App-title">Prime Pizza</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/customerInfo">Customer Info</Link>
+        </li>
+        <li>
+          <Link to="/Menu">Menu</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
